0c68a1bb478637cae5d3f7154038b7f488e2d9e9
Aleksanra1 <149382178+Aleksanra1@users.noreply.github.com>
2023-10-30 14:45:31 +0100

    update read funcionalities

---
+++ dane.py
@@ -0,0 +1,11 @@
+users_list = [
+    {"name":"Ola", "nick":"major", "posts":1},
+    {"name":"Kinga", "nick":"Kinia","posts":60},
+    {"name":"Bartosz", "nick":"Baran", "posts":31231},
+    {"name":"Kamil", "nick":"Koc","posts":123},
+    {"name":"Marek", "nick":"Wiss","posts":0},
+    {"name":"Wiktoria", "nick":"Witka","posts":360},
+    {"name":"Wiktoria", "nick":"Wiki","posts":123},
+    {"name":"Monika", "nick":"Monia","posts":1_000_000},
+    {"name":"Kasia", "nick":"Kkk","posts":1500},
+]
---
+++ main.py
@@ -0,0 +1,8 @@
+from dane import users_list
+
+for user in users_list:
+    print(f'Twoj znajomy {user["nick"]} dodał {user["posts"]}')
+
+# print(f'Twój znajomy {zmienna_na_dane[0]["nick"]} opublikował {zmienna_na_dane[0]["posts"]}!!!')
+
+
