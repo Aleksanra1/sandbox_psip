1a8a87ddb75a9dcf59c2ac3ade0e1c60841ad8cb
Aleksanra1 <149382178+Aleksanra1@users.noreply.github.com>
2023-11-06 14:02:30 +0100

    chore: add remove user function

--- dane.py
+++ dane.py
@@ -1,5 +1,5 @@
 users_list: list = [
-    {"name":"Ola", "nick":"major", "posts":1},
+    {"name":"Marek", "nick":"major", "posts":1},
     {"name":"Kinga", "nick":"Kinia","posts":60},
     {"name":"Bartosz", "nick":"Baran", "posts":31231},
     {"name":"Kamil", "nick":"Koc","posts":123},
@@ -9,5 +9,4 @@ users_list: list = [
     {"name":"Monika", "nick":"Monia","posts":1_000_000},
     {"name":"Kasia", "nick":"Kkk","posts":1500},
     {"name":"Agata", "nick":"Aaa","posts":1500},
-
 ]
